b1cc07631fa8957c90b90e3c2790fb3f96c99dae
Hyehyun1001 <hye22327@naver.com>
2024-06-13 10:25:18 +0900

    메뉴 및 칼로리 수정

--- src/diet_project/kiosk.java
+++ src/diet_project/kiosk.java
@@ -13,10 +13,10 @@ public class kiosk {
 
         TreeMap<String, Integer> information;
 
-        List<String> listKR = Arrays.asList("고기", "밥", "김치","국밥");
-        List<String> listJP = Arrays.asList("초밥","우동","돈가쓰");
-        List<String> listCH = Arrays.asList("짜장면","탕수육");
-        List<String> listEN = Arrays.asList("햄버거","피자");
+        List<String> listKR = Arrays.asList("고기","밥","김치볶음밥","미역국","제육덮밥","떡볶이");
+        List<String> listJP = Arrays.asList("초밥","우동","소바","돈카츠","타코야끼","오코노미야끼");
+        List<String> listCH = Arrays.asList("짜장면","짬뽕","탕수육","유린기","동파육");
+        List<String> listEN = Arrays.asList("햄버거","피자","파스타","바비큐","핫도그","리조또");
 
 
 
@@ -26,23 +26,36 @@ public class kiosk {
         kiosk.put("양식",listEN);
 
 
-        //KR
-        kioskKR.put("고기",312);
-        kioskKR.put("밥",31);
-        kioskKR.put("김치",51);
-        kioskKR.put("국밥",123123);
-        //JP
-        kioskJP.put("초밥",62);
-        kioskJP.put("우동",122);
-        kioskJP.put("돈가쓰",111);
+        // KR
+        kioskKR.put("고기", 500);
+        kioskKR.put("밥", 200);
+        kioskKR.put("김치볶음밥", 600);
+        kioskKR.put("미역국", 100);
+        kioskKR.put("제육덮밥", 700);
+        kioskKR.put("떡볶이", 300);
 
-        //CH
-        kioskCH.put("짜장면",222);
-        kioskCH.put("탕수육",333);
+        // JP
+        kioskJP.put("초밥", 350);
+        kioskJP.put("우동", 400);
+        kioskJP.put("소바", 300);
+        kioskJP.put("돈카츠", 600);
+        kioskJP.put("타코야끼", 500);
+        kioskJP.put("오코노미야끼", 600);
 
-        //EN
-        kioskEN.put("햄버거",123);
-        kioskEN.put("피자",1);
+        // CH
+        kioskCH.put("짜장면", 700);
+        kioskCH.put("짬뽕", 600);
+        kioskCH.put("탕수육", 800);
+        kioskCH.put("유린기", 700);
+        kioskCH.put("동파육", 750);
+
+        // EN
+        kioskEN.put("햄버거", 500);
+        kioskEN.put("피자", 600);
+        kioskEN.put("파스타", 700);
+        kioskEN.put("바비큐", 800);
+        kioskEN.put("핫도그", 400);
+        kioskEN.put("리조또", 650);
 
         String minKey = null;
         int minValue = Integer.MAX_VALUE;
